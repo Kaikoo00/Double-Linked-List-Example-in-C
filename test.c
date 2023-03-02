8c8fbf77bb6d26a5ef8b83f6e2a00a3fc125c3d2
Kaikoo00 <116528497+Kaikoo00@users.noreply.github.com>
2023-03-02 18:18:08 +0700

    Add files via upload

---
+++ test.c
@@ -0,0 +1,154 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node{
+	int x;
+	struct node *next;
+	struct node *prev;
+};
+struct node *head,*tail;
+int data_count;
+
+void CreateLinkedList(int value){
+	printf("Inputted Value: %d\n", value);
+	struct node *newnode =(struct node*)malloc(sizeof(struct node));
+	newnode->x=value;
+	newnode->next=NULL;
+	head=newnode;
+	tail=head;
+	head->next=tail;head->prev=NULL;
+	tail->prev=head;tail->next=NULL;
+}
+
+void insert_head(int value){
+	printf("Inputted Value: %d\n", value);
+	struct node *newnode =(struct node*)malloc(sizeof(struct node));
+	newnode->x = value;
+	head->prev=newnode;
+	newnode->next = head;
+	head = newnode;
+}
+
+void insert_tail(int value){
+	printf("Inputted Value: %d\n", value);
+	struct node *newnode =(struct node*)malloc(sizeof(struct node));
+	newnode->x = value;
+	newnode->next = NULL;
+	newnode->prev = tail;
+	tail->next=newnode;
+	tail = newnode;
+}
+
+void insert_midList(int pos, int value){
+	if(pos==0){
+		insert_head(value);
+		return;
+	}
+
+	if(pos>data_count){
+		printf("Invalid position\n");
+		return;
+	}
+
+	if(pos==data_count){
+		insert_tail(value);
+		return;
+	}
+	printf("Inputted Value: %d || Index : %d\n", value, pos);
+	struct node *newnode = (struct node*)malloc(sizeof(struct node));
+	newnode->x = value;
+	struct node *curr, *prev;
+	curr = head;
+	for(int i = 0; i<pos; i++){
+		prev = curr;
+		curr = curr->next;
+	}
+	prev->next = newnode;
+	newnode->prev = prev;
+	newnode->next = curr;
+	curr->prev = newnode;
+}
+
+void delete_head(){
+	printf("Deleted item: %d\n", head->x);
+	head = head->next;
+	head->prev = NULL;
+}
+
+void delete_tail(){
+	printf("Deleted item : %d\n", tail->x);
+	tail = tail->prev;
+	tail->next = NULL;
+}
+
+void delete_midList(int pos){
+	if(pos==0){
+		delete_head();
+		return;
+	}
+
+	if(pos>=data_count){
+		printf("Invalid Position\n");
+		return;
+	}
+
+	if(pos==data_count-1){
+		delete_tail();
+		return;
+	}
+
+	struct node *curr, *prev, *next;
+	curr = head;
+	for(int i = 0; i<pos; i++){
+		prev = curr;
+		curr = curr->next;
+		next = curr->next;
+	}
+	printf("Deleted Item: %d", curr->x);printf(" || Index : %d\n", pos);
+	prev->next = next;
+	next->prev = prev;
+	free(curr);
+}
+
+int main()
+{
+    CreateLinkedList(1);data_count++;
+	insert_tail(2);data_count++;
+	insert_tail(3);data_count++;
+	insert_midList(2, 4);data_count++;
+	printf("Print list using head as reference point:\n");
+	printf("%d %d %d %d\n", head->x, head->next->x, head->next->next->x, head->next->next->next->x);
+	printf("Print list using tail as reference point:\n");
+	printf("%d %d %d %d\n\n", tail->prev->prev->prev->x, tail->prev->prev->x, tail->prev->x, tail->x);
+	
+	printf("Check if the input exception works: \n");
+	insert_midList(5, 10);
+	printf("\n");
+
+	delete_head();data_count--;
+	printf("Print list using head as reference point:\n");
+	printf("%d %d %d\n", head->x, head->next->x, head->next->next->x);
+	printf("Print list using tail as reference point:\n");
+	printf("%d %d %d\n\n", tail->prev->prev->x, tail->prev->x, tail->x);
+
+	delete_tail();data_count--;
+	printf("Print list using head as reference point:\n");
+	printf("%d %d\n", head->x, head->next->x);
+	printf("Print list using tail as reference point:\n");
+	printf("%d %d\n\n", tail->prev->x, tail->x);
+
+	insert_tail(3);data_count++;
+	printf("Input check:\n");
+	printf("%d %d %d %d\n\n", head->x, head->next->x, tail->prev->x, tail->x);
+
+	printf("Check if delete exception works:\n");
+	delete_midList(10);
+	printf("\n");
+
+	delete_midList(1);data_count--;
+	printf("Print list using head as reference point:\n");
+	printf("%d %d\n", head->x, head->next->x);
+	printf("Print list using tail as reference point:\n");
+	printf("%d %d\n", tail->prev->x, tail->x);
+    return 0;
+}
